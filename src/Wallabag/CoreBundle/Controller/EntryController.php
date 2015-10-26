50b24ee774d8a00b95745c9a19680a6b7785b13b
Nicolas Lœuillet <nicolas.loeuillet@smile.fr>
2015-10-26 14:38:24 +0100

    fix #1502 avoid duplicate entry and store pocket url in config

--- src/Wallabag/CoreBundle/Controller/EntryController.php
+++ src/Wallabag/CoreBundle/Controller/EntryController.php
@@ -40,6 +40,7 @@ class EntryController extends Controller
      */
     public function addEntryFormAction(Request $request)
     {
+        $em = $this->getDoctrine()->getManager();
         $entry = new Entry($this->getUser());
 
         $form = $this->createForm(new NewEntryType(), $entry);
@@ -47,6 +48,19 @@ class EntryController extends Controller
         $form->handleRequest($request);
 
         if ($form->isValid()) {
+            $existingEntry = $em
+                ->getRepository('WallabagCoreBundle:Entry')
+                ->findOneByUrl($entry->getUrl());
+
+            if (!is_null($existingEntry)) {
+                $this->get('session')->getFlashBag()->add(
+                    'notice',
+                    'Entry already saved on '.$existingEntry->getCreatedAt()->format('d-m-Y')
+                );
+
+                return $this->redirect($this->generateUrl('view', array('id' => $existingEntry->getId())));
+            }
+
             $this->updateEntry($entry);
             $this->get('session')->getFlashBag()->add(
                 'notice',
